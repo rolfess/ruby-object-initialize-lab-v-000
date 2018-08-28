9eeed89438befe850d299c96608b7d71cdadfe5c
Susan Rolfes <rolfes57069@yahoo.com>
2018-08-28 21:11:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,12 @@ class Dog
   def initialize (name)
     @name = name
   end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
 
   def initialize (breed)
     @breed = "Mutt"
